92b99881f1555f805d2717d269a216f3c9cf46f3
holgerschurig <holgerschurig>
2001-11-29 15:42:50 +0000

    changed to default server name

--- els/els-samba/logon.bat
+++ els/els-samba/logon.bat
@@ -2,14 +2,11 @@
 
 echo Welcome to the Easy Linux Server !
 echo.
-net use F: \\LINUX\APP
-net use G: \\LINUX\DAT
-net use H: \\LINUX\HOME
-net use I: \\LINUX\DAT
-net time \\LINUX /set /yes
-
-rem nbtstat -s >>G:\TRANSFER\STATUS.TXT
-rem mem        >>G:\TRANSFER\STATUS.TXT
+net use F: \\SERVER\APP
+net use G: \\SERVER\DAT
+net use H: \\SERVER\HOME
+net use I: \\SERVER\DAT
+net time \\SERVER /set /yes
 
 if exist h:logon.bat h:logon.bat
 
