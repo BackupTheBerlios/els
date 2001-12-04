84a69186d809f0e88421367acd57120e3fecff7a
holgerschurig <holgerschurig>
2001-12-04 22:36:41 +0000

    profile.setup was not necessary, because we have the profile.d dropin directory

---
+++ els/els-base/els-base.sh
@@ -0,0 +1,10 @@
+# several handy paths
+export TEMP=/var/tmp
+export TMP=/var/tmp
+export TMPDIR=/var/tmp
+
+# vars for BASH and LESS
+export IGNOREEOF=0
+export HISTCONTROL=ignoreboth
+export FIGNORE=.a:.o
+export LESS="-MSisch4"
