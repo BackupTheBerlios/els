da66125a7003da6d7903ffd0bf1298fb488caabe
holgerschurig <holgerschurig>
2001-12-05 09:19:06 +0000

    added handling of links and CVS id

--- els/utilities/text2shtml.pl
+++ els/utilities/text2shtml.pl
@@ -15,6 +15,10 @@ a shtml file.
 
 use strict;
 
+
+my $rel = $ENV{ROOT} || '../';
+$rel = substr($rel, 3) . '/';
+
 sub ProcessFile ($)
 {
     my $fname = shift;
@@ -25,33 +29,41 @@ sub ProcessFile ($)
     my $lines = <FILE>;
     $lines = "\n$lines\n";
 
-    # Lines underlined by ====  ->  H2
-    # Lines underlined by ----  ->  H3
+    # CVS ID				-> delete
+    $lines =~ s{\$Id.+?\$}{}g;
+
+    # Lines underlined by ====		->  H2
+    # Lines underlined by ----		->  H3
     $lines =~ s{\n(.+?)\n=+\n}{\n<H2>$1</H2>\n\n}g;
     $lines =~ s{\n(.+?)\n-+\n}{\n<H3>$1</H3>\n\n}g;
 
-    # Lines with -* at the front -> LI
-    # Start of <LI> lines        -> UL
-    # End of <LI> lines          -> /UL
+    # Lines with -* at the front	-> LI
+    # Start of <LI> lines		-> UL
+    # End of <LI> lines			-> /UL
     $lines =~ s{\n[\*-]\s+(.+?)}{\n<LI>$1}g;
     $lines =~ s{\n\n<LI>}{\n\n<UL>\n<LI>}g;
     $lines =~ s{<LI>(.+?)\n\n}{<LI>$1\n</UL>\n\n}g;
 
-    # Two empty lines            -> one empty line
-    # One empty line             -> P
-    # End of paragraph           -> /P
-    # <P> without prior </P>     -> /P P
+    # Two empty lines			-> one empty line
+    # One empty line			-> P
+    # End of paragraph			-> /P
+    # <P> without prior </P>		-> /P P
     $lines =~ s{\n\n\n}{\n\n}g;
     $lines =~ s{\n\n([^<\n])}{\n<P>\n$1}g;
     $lines =~ s{([^>])\n\n}{$1\n</P>\n}g;
     $lines =~ s{([^>])\n<P>\n}{$1\n</P><P>\n}g;
 
-    # Indented line              -> TT
-    # No clue on while I need to double this
+    # Indented line			-> TT
+    # It might be a perl bug, but this line has to be executed twice ...
     $lines =~ s{\n   (.+)\n}{\n<TT> &nbsp; $1</TT><BR>\n}g;
     $lines =~ s{\n   (.+)\n}{\n<TT> &nbsp; $1</TT><BR>\n}g;
 
-    # Newline at start and end   -> get rid of it
+    # word (see website/url)		-> A HREF
+    # "words" (seet website/url)	-> A HREF
+    $lines =~ s{([^"\s]+)\s+\(see website/(.+?)\)}{<A HREF="$rel$2">$1</A>}g;
+    $lines =~ s{"(.+?)"\s+\(see website/(.+?)\)}{<A HREF="$rel$2">$1</A>}g;
+
+    # Newline at start and end		-> get rid of it
     $lines =~ s{^\s+}{}g;
     $lines =~ s{\s+$}{}g;
 
