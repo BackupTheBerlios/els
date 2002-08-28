2868fc492163ad9d916e9bb034dd42efc9ead855
holgerschurig <holgerschurig>
2002-08-28 10:21:11 +0000

    added 'insertafter'

--- els/els-base/ConfigFiles.pm
+++ els/els-base/ConfigFiles.pm
@@ -3,29 +3,30 @@ require      Exporter;
 @ISA       = qw(Exporter); 
 @EXPORT    = qw(loadfile printfile writefile deleteall
                 comment commentanywhere commentonce
-		uncomment uncommentanywhere uncommentonce
+                uncomment uncommentanywhere uncommentonce
                 append prepend appendonce prependonce
+                insertafter
                 setopt getopt
                 search replace replaceonce
                 deleteline
-		searchline
+                searchline
 
-	        ActivateSample
+                ActivateSample
 
-		%users %groups
-		ReadPasswd ReadGroup ReadData
-		WritePasswd WriteGroup WriteData
-		GetGroupName);
+                %users %groups
+                ReadPasswd ReadGroup ReadData
+                WritePasswd WriteGroup WriteData
+                GetGroupName);
 @EXPORT_OK = qw($dobackup @lines);
 
-@lines	= ();
+@lines        = ();
 %users  = ();
 %groups = ();
 
 
 
 # LOADFILE
-#	loads a file into memory
+#        loads a file into memory
 sub loadfile ($;$) {
     ($file, $header) = @_;
     @lines = ();
@@ -33,7 +34,7 @@ sub loadfile ($;$) {
 };
 
 # PRINTFILE
-#	prints the current memory-image of the file to stdout
+#        prints the current memory-image of the file to stdout
 sub printfile () {
     print "File \"$file\":\n";
     print @lines;
@@ -41,7 +42,7 @@ sub printfile () {
 }
 
 # WRITEFILE
-#	supposed to write the file back onto the disk
+#        supposed to write the file back onto the disk
 sub writefile () {
     # let's create a backup of the initial file
     chomp($filename = `basename $file`);
@@ -55,29 +56,29 @@ sub writefile () {
 }
 
 # DELETEALL
-#	delete all lines
+#        delete all lines
 sub deleteall () {
     @lines = ();
 }
 
 # COMMENT 
-# 	comment out a line, pattern must match at start of line
+#         comment out a line, pattern must match at start of line
 sub comment ($) {
     $pattern = shift(@_);
     #
     # The used regular expression means:
     #
-    # ^		start at beginning of line
-    # ()	the matched string will be stored in $1
-    # $pattern	will be substituted by the argument
-    # .*	zero or more additional characters allowed
+    # ^                start at beginning of line
+    # ()        the matched string will be stored in $1
+    # $pattern        will be substituted by the argument
+    # .*        zero or more additional characters allowed
     #
     $_ = grep { s/^($pattern.*)/#$1/ } @lines;
     return not $_;
 }
 
 # COMMENTANYWHERE
-# 	comment out a line, pattern can match anywhere
+#         comment out a line, pattern can match anywhere
 sub commentanywhere ($) {
     $pattern = shift(@_);
     # () the matched string will be stored in $1
@@ -89,17 +90,17 @@ sub commentanywhere ($) {
 }
 
 # COMMENTONCE
-# 	comment first line, pattern must match at start of line
+#         comment first line, pattern must match at start of line
 sub commentonce ($) {
     $pattern = shift(@_);
     for (@lines) {
-	last if s/^($pattern.*$)/#$1/;
+        last if s/^($pattern.*$)/#$1/;
     }
     return not $_;
 }
 
 # UNCOMMENT 
-# 	uncomment a line, pattern must match at start of line
+#         uncomment a line, pattern must match at start of line
 sub uncomment ($) {
     $pattern = shift(@_);
     $_ = grep { s/^#[ \t]*($pattern.*)/$1/ } @lines;
@@ -107,7 +108,7 @@ sub uncomment ($) {
 }
 
 # UNCOMMENTANYWHERE
-# 	uncomment a line, pattern can match anywhere
+#         uncomment a line, pattern can match anywhere
 sub uncommentanywhere ($) {
     $pattern = shift(@_);
     $_ = grep { s/^#[ \t]*(.*$pattern.*)/$1/ } @lines;
@@ -115,23 +116,23 @@ sub uncommentanywhere ($) {
 }
 
 # UNCOMMENTONCE
-# 	uncomment first line, pattern must match at start of line
+#         uncomment first line, pattern must match at start of line
 sub uncommentonce ($) {
     $pattern = shift(@_);
     foreach (@lines) {
-	last if s/^#[ \t]*(.*$pattern.*)/$1/;
+        last if s/^#[ \t]*(.*$pattern.*)/$1/;
     }
 }
 
 # APPEND
-# 	append line at the end
+#         append line at the end
 sub append ($) {
     $what = shift(@_);
     push @lines, $what . "\n";
 }
 
 # APPENDONCE
-# 	appendonce line if not already present
+#         appendonce line if not already present
 sub appendonce ($) {
     $what = shift(@_);
     $_ = grep { /$what/ } @lines or push @lines, $what . "\n";
@@ -139,23 +140,33 @@ sub appendonce ($) {
 }
 
 # PREPEND
-# 	inserts a line at the beginning
+#         inserts a line at the beginning
 sub prepend ($) {
     $what = shift(@_);
     unshift @lines, $what . "\n";
 }
 
 # PREPENDONCE
-# 	inserts a line at the beginning if not already present
+#         inserts a line at the beginning if not already present
 sub prependonce ($) {
     $what = shift(@_);
     $_ = grep { /$what/ } @lines or unshift @lines, $what . "\n";
     return not $_;
 }
 
+# INSERTAFTER
+#        Search for a pattern, insert line after first line found
+sub insertafter($$) {
+    ($pattern,$line) = @_;
+    $n = searchline($pattern);
+    if (defined $n) {
+       @lines = ( @lines[0..$n], "$line\n", @lines[$n+1..$#lines] );
+    }
+}
+
 # SETOPT
-#	set option, even if commented out
-#	if option is not already present, put it into the file
+#        set option, even if commented out
+#        if option is not already present, put it into the file
 sub setopt ($$) {
     ($pattern, $option) = @_;
     $_ = grep { s/^([# \t])*($pattern).*/$2$option/ } @lines   or appendonce $pattern . $option;
@@ -166,13 +177,13 @@ sub setopt ($$) {
 sub getopt ($) {
     $pattern = shift;
     foreach (@lines) {
-	if (/^$pattern(.*)$/) { return $1 };
+        if (/^$pattern(.*)$/) { return $1 };
     }
     return '';
 }
 
 # SEARCH
-#	returns whether a string has been found or not
+#        returns whether a string has been found or not
 sub search ($) {
     $pattern = shift(@_);
     my $line;
@@ -181,7 +192,7 @@ sub search ($) {
 }
 
 # REPLACE
-#	replaces each string with another one
+#        replaces each string with another one
 sub replace ($$) {
     ($search, $replace) = @_;
     $_ = grep { s/$search/$replace/g } @lines;
@@ -189,30 +200,30 @@ sub replace ($$) {
 }
 
 # REPLACEONCE
-#	replaces the first occurrence of a string with another one
+#        replaces the first occurrence of a string with another one
 sub replaceonce ($$) {
     ($search, $replace) = @_;
     foreach $_ ( @lines ) {
-	last if s/$search/$replace/g;
+        last if s/$search/$replace/g;
     }
 }
 
 # DELETELINE
-#	deletes an entire line
+#        deletes an entire line
 sub deleteline ($) {
     $pattern = shift(@_);
     @lines = map { if ($_ =~ /$pattern/) { } else { $_ } } @lines;
 }
 
 # SEARCHLINE
-#	searches a line in the array and returns the number of the first
-#	occurence
+#        searches a line in the array and returns the number of the first
+#        occurence
 sub searchline ($) {
     $pattern = shift(@_);
     $number = 0;
     foreach (@lines) {
-	last if /$pattern/;
-	$number++
+        last if /$pattern/;
+        $number++
     }
     $number = undef if ($number > $#lines);
     return $number;
@@ -221,8 +232,8 @@ sub searchline ($) {
 
 #
 # ActivateSample
-#	if a file  x.sample  exists but the file  x  not, then the
-#	x  file will be created by copying  x.sample
+#        if a file  x.sample  exists but the file  x  not, then the
+#        x  file will be created by copying  x.sample
 sub ActivateSample($)
 {
     my($file) = @_;
@@ -245,9 +256,9 @@ sub ActivateSample($)
 # To access a single field, e.g. the full name of user holger, use
 # $users{"holger"}[3] = "Holger Schurig". Here are all fields:
 #
-# $users{$name}[0] password		$groups{$name}[0] group-password
-# $users{$name}[1] user id		$groups{$name}[1] group id
-# $users{$name}[2] primary group id	$groups{$name}[2] member list
+# $users{$name}[0] password                $groups{$name}[0] group-password
+# $users{$name}[1] user id                $groups{$name}[1] group id
+# $users{$name}[2] primary group id        $groups{$name}[2] member list
 # $users{$name}[3] full name
 # $users{$name}[4] home path
 # $users{$name}[5] shell
@@ -257,7 +268,7 @@ sub ActivateSample($)
 
 #
 # READPASSWD
-#	Reads /etc/passwd and put the data into the %users hash
+#        Reads /etc/passwd and put the data into the %users hash
 #
 sub ReadPasswd ()
 {
@@ -266,18 +277,18 @@ sub ReadPasswd ()
     $FILE = "/etc/passwd";
     open FILE or die "Can't open $FILE";
     while (<FILE>) {
-	chomp;
-	@_ = split /:/, $_;
-	$name = shift @_;
-	$users{$name} = [ @_ ];
-	unless ($users{$name}[5]) { $users{$name}[5] = ""; }
+        chomp;
+        @_ = split /:/, $_;
+        $name = shift @_;
+        $users{$name} = [ @_ ];
+        unless ($users{$name}[5]) { $users{$name}[5] = ""; }
     }
 }
 
 
 #
 # READGROUP
-#	Reads /etc/group and put the data into the %groups hash
+#        Reads /etc/group and put the data into the %groups hash
 #
 sub ReadGroup ()
 {
@@ -286,18 +297,18 @@ sub ReadGroup ()
     $FILE = "/etc/group";
     open FILE or die "Can't open $FILE";
     while (<FILE>) {
-	chomp;
-	@_ = split ":", $_;
-	$name = shift;
-	$groups{$name} = [ @_ ];
-	unless ($groups{$name}[2]) { $groups{$name}[2] = ""; }
+        chomp;
+        @_ = split ":", $_;
+        $name = shift;
+        $groups{$name} = [ @_ ];
+        unless ($groups{$name}[2]) { $groups{$name}[2] = ""; }
     }
 }
 
 
 #
 # READDATA
-#	Reads the whole user database (/etc/passwd and /etc/group).
+#        Reads the whole user database (/etc/passwd and /etc/group).
 #
 sub ReadData ()
 {
@@ -308,8 +319,8 @@ sub ReadData ()
 
 #
 # WRITEPASSWD
-#	Writes user data from %users to /etc/passwd.
-#	Creates a backup file "/etc/passwd-".
+#        Writes user data from %users to /etc/passwd.
+#        Creates a backup file "/etc/passwd-".
 #
 sub WritePasswd ()
 {
@@ -322,15 +333,15 @@ sub WritePasswd ()
 
     my $name;
     foreach $name ( sort { $users{$a}[1] <=> $users{$b}[1] } keys %users ) {
-	print FILE "$name:" , (join ":", @{ $users{$name} }) , "\n";
+        print FILE "$name:" , (join ":", @{ $users{$name} }) , "\n";
     }
 }
 
 
 #
 # WRITEGROUP
-#	Writes group data from %groups to /etc/group.
-#	Creates a backup file "/etc/group-".
+#        Writes group data from %groups to /etc/group.
+#        Creates a backup file "/etc/group-".
 #
 sub WriteGroup ()
 {
@@ -343,14 +354,14 @@ sub WriteGroup ()
 
     my $name;
     foreach $name ( sort { $groups{$a}[1] <=> $groups{$b}[1] } keys %groups) {
-	print FILE "$name:" , (join ":", @{ $groups{$name} }) , "\n";
+        print FILE "$name:" , (join ":", @{ $groups{$name} }) , "\n";
     }
 }
 
 
 #
 # WRITEDATA
-#	Writes both %users and %groups to /etc/passwd and /etc/group.
+#        Writes both %users and %groups to /etc/passwd and /etc/group.
 #
 sub WriteData ()
 {
@@ -361,16 +372,16 @@ sub WriteData ()
 
 #
 # GETGROUPNAME
-#	returns group name for a given group id
+#        returns group name for a given group id
 #
 sub GetGroupName ($)
 {
     my($id) = @_;
 
     foreach $_ ( keys %groups ) {
-	if ($groups{$_}[1] eq $id) {
-	    return $_;
-	}
+        if ($groups{$_}[1] eq $id) {
+            return $_;
+        }
     }
 
     # Not found?
